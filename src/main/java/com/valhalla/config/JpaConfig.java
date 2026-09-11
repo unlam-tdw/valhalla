81ece4c6f3e8f2fc8ff5f905e514c81020e22351
Mateo Maccarone <maccaronemateo.dev@gmail.com>
2026-09-10 21:08:06 -0300

    perf: skip quality checks in dev (-Pdev), remove explicit hibernate.dialect

--- src/main/java/com/valhalla/config/JpaConfig.java
+++ src/main/java/com/valhalla/config/JpaConfig.java
@@ -23,7 +23,6 @@ public class JpaConfig extends BaseJpaConfig {
   @Override
   protected Properties jpaProperties() {
     Properties properties = new Properties();
-    properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
     properties.setProperty("hibernate.show_sql", "true");
     properties.setProperty("hibernate.format_sql", "true");
     properties.setProperty("hibernate.hbm2ddl.auto", "update");
